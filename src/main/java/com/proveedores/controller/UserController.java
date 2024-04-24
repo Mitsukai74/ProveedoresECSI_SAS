5760ef9bb958fec10643f75bcf63a4600865f32d
Mitsukai74 <ansaanva@gmail.com>
2024-04-23 20:52:34 -0500

    Adecuación de formulario de nuevo usuario y del controller con su botón de guardado

--- src/main/java/com/proveedores/controller/UserController.java
+++ src/main/java/com/proveedores/controller/UserController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.proveedores.models.Usuarios;
@@ -30,10 +32,24 @@ public class UserController {
 	}
 	
 	@GetMapping("/create")
-	public String crearUser(){
+	public String crearUser(Model model){
+		
+		Usuarios usuario = new Usuarios();
+		
+		model.addAttribute("titulo","Formulario: Nuevo usuario");
+		model.addAttribute("usuario",usuario);
 		
 		
 		return "/usuarios/formCrearUser";
+	}
+	
+	@PostMapping("/save")
+	public String guardar(@ModelAttribute Usuarios usuario){
+		
+		usuarioServicio.guardar(usuario);		
+		System.out.println("Cliente guardado");
+		
+		return "redirect:/usuarios/";
 	}
 	
 
